b1402c2026ef4626b10399a81b472be49f31baa6
Hayata Suenaga <98560306+hayata-suenaga@users.noreply.github.com>
2022-12-05 20:56:53 -0500

    Create loading and error indicators

---
+++ components/Errored.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const Errored = () => {
+  return (
+    <View>
+      <Text>Errored</Text>
+    </View>
+  );
+};
+
+export default Errored;
+
+const styles = StyleSheet.create({});
