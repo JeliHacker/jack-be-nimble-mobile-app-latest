77b457f6f535a81ebb01e1b4453c7eac1fcfdf1a
Hayata Suenaga <98560306+hayata-suenaga@users.noreply.github.com>
2022-12-08 12:38:41 -0500

    Remove algolia client

--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,17 +1,9 @@
 import { Button, StyleSheet, Text, View } from 'react-native';
 import { BottomTabScreenProps } from '../navigation/types';
-import SearchBox from './SearchBox';
-import SearchConfigure from './SearchConfigure';
-import SearchHits from './SearchHits';
 
 const Search = ({ navigation }: BottomTabScreenProps<'Search'>) => {
   return (
     <View style={styles.container}>
-      <SearchConfigure hitsPerPage={5} />
-      <SearchBox />
-      <View style={{ backgroundColor: 'red' }}>
-        <SearchHits />
-      </View>
       <Button
         title="go to term"
         onPress={() => navigation.navigate('Term', { termId: 'id' })}
--- screens/SearchConfigure.tsx
+++
@@ -1,8 +0,0 @@
-import { useConfigure } from 'react-instantsearch-hooks';
-
-const SearchConfigure = (props: any) => {
-  useConfigure(props);
-  return null;
-};
-
-export default SearchConfigure;
